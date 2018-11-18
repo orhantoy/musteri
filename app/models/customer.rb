59fff1ef7bdd30fbfa9f35f3942ccea8f771f550
Orhan Toy <toyorhan@gmail.com>
2018-11-17 20:54:36 -0800

    Show country name for persisted customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,11 @@
 class Customer < ApplicationRecord
   belongs_to :space
+
+  def country
+    ISO3166::Country.new(country_code) if country_code?
+  end
+
+  def country_name
+    country.name if country
+  end
 end
